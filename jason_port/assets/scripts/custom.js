47ddb10d5ba5423b771a47afa4ed86fd196fe0a1
Jason Ou <jasonou95@gmail.com>
2015-07-27 04:45:40 -0700

    added Built Filters

--- jason_port/assets/scripts/custom.js
+++ jason_port/assets/scripts/custom.js
@@ -14,3 +14,36 @@ $(document).ready(function(){
             opacity: (back) ? 1 : 0.5
         }, 700, function(){pulse(!back)});
 })(false);
+
+/* Isotope Filter */
+$( function() {
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+    });
+    
+    // filter functions
+    var filterFns = {
+        // show if name ends with -ium
+        ium: function() {
+            var name = $(this).find('.name').text();
+            return name.match( /ium$/ );
+        }
+    };
+    // bind filter button click
+    $('.filters-button-group').on( 'click', 'button', function() {
+        var filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+        });
+    });
+});
