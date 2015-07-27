4a1575826f00a3e502ac95a5678363b04424afdf
Jason Ou <jasonou95@gmail.com>
2015-07-27 05:22:12 -0700

    working filters

--- jason_port/assets/scripts/custom.js
+++ jason_port/assets/scripts/custom.js
@@ -16,11 +16,15 @@ $(document).ready(function(){
 })(false);
 
 /* Isotope Filter */
-$( function() {
+$(window).load( function() {
     // init Isotope
     var $grid = $('.grid').isotope({
-        itemSelector: '.item',
-        layoutMode: 'fitRows'
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
     });
     
     // filter functions
@@ -31,6 +35,7 @@ $( function() {
             return name.match( /ium$/ );
         }
     };
+    
     // bind filter button click
     $('.filters-button-group').on( 'click', 'button', function() {
         var filterValue = $( this ).attr('data-filter');
@@ -38,9 +43,11 @@ $( function() {
         filterValue = filterFns[ filterValue ] || filterValue;
         $grid.isotope({ filter: filterValue });
     });
+    
     // change is-checked class on buttons
     $('.button-group').each( function( i, buttonGroup ) {
         var $buttonGroup = $( buttonGroup );
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
         $buttonGroup.on( 'click', 'button', function() {
             $buttonGroup.find('.is-checked').removeClass('is-checked');
             $( this ).addClass('is-checked');
