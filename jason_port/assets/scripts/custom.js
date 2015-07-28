384717b03d756bc127ba8e0a19b1e8f379e8c5aa
Jason Ou <jasonou95@gmail.com>
2015-07-28 15:48:47 -0700

    added Did you know

--- jason_port/assets/scripts/custom.js
+++ jason_port/assets/scripts/custom.js
@@ -54,3 +54,38 @@ $(window).load( function() {
         });
     });
 });
+
+var images = {
+    1: [
+        {
+            href : 'http://fancyapps.com/fancybox/demo/1_b.jpg',                
+            title : 'Gallery 1 - 1'
+        },
+        {
+            href : 'http://fancyapps.com/fancybox/demo/2_b.jpg',                
+            title : 'Gallery 1 - 2'
+        },
+        {
+            href : 'http://fancyapps.com/fancybox/demo/3_b.jpg',                
+            title : 'Gallery 1 - 3'
+        }
+    ],
+    2: [
+        {
+            href : 'http://fancyapps.com/fancybox/demo/4_b.jpg',                
+            title : 'Gallery 2 - 1'
+        },
+        {
+            href : 'http://fancyapps.com/fancybox/demo/5_b.jpg',                
+            title : 'Gallery 2 - 2'
+        }
+    ]
+};
+
+$(".open_fancybox").click(function() {
+    $.fancybox.open(images[ $(this).index() + 1], {
+        padding : 0
+    });
+    
+    return false;
+});
